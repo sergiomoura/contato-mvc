a0097d72cf583fcbab28fecc1a2b085b1fa08c13
Sérgio Moura <smouracalmon@gmail.com>
2022-04-13 15:55:47 -0300

    Fase 2 - Criando e levantando um servidor express

---
+++ app.js
@@ -0,0 +1,14 @@
+// Importando express
+const express = require('express');
+
+// Criando servidor e armazenando em app
+const app = express();
+
+// Criando rota get para '/' que responde "Olá"
+app.get('/',(_req,res)=>{res.send("Olá!")});
+
+// Rodando o servidor
+app.listen(3000, ()=>{
+    console.log("Servidor escutando na porta 3000")
+});
+
