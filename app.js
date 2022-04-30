4f47eddb08ab4cc401981f5c9e9ad0c6e7cde7fb
Sérgio Moura <smouracalmon@gmail.com>
2022-04-30 17:06:01 -0300

    Fase 10.1 - Instalando e configurando o express-session

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 // Importando express
 const express = require('express');
 
+// Importando o express-session
+const session = require('express-session');
+
 // Importando roteadores
 const UsuariosRouter = require("./routers/UsuariosRouter");
 const ContatosRouter = require("./routers/ContatosRouter");
 
 // Criando servidor e armazenando em app
 const app = express();
+
+// Configurando o middleware que carrega sessions a partir do cookie, caso haja.
+app.use(session({
+    secret: "SEGREDO",
+    resave: false,
+    saveUninitialized: false
+}))
 
 // Interpretando os formulários que vierem via post com enctype www/x-form-urlencoded 
 app.use(express.urlencoded({ extended: false }));
