816d0d85852b542a002bfcd35d4a91dd72f209b1
Sérgio Moura <smouracalmon@gmail.com>
2022-05-01 04:46:54 -0300

    Fase 12.4 - Carregando contatos de usuario e enviando para a view

--- controllers/ContatosController.js
+++ controllers/ContatosController.js
@@ -1,5 +1,24 @@
+const ContatosServices = require("../services/ContatosServices");
+
 module.exports = {
     listarContatos: (req, res) => {
-        res.send("Aqui virá a lista de contatos...");
+
+        // Capturando o id do usuário logado
+        let id = req.session.usuario.id;
+
+        try {
+
+            // Carregando os contatos do usuário
+            let contatos = ContatosServices.carregar(id);
+    
+            // Enviando para a view
+            res.render('contatos.ejs',{contatos});
+
+        } catch (error) {
+
+            // Enviando mensagem de erro para o visitante
+            res.send(error);
+
+        }
     }
 }
